fb38e9243e1fe85caf33590d13f807fd200daffd
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-03-29 06:56:24 +0800

    image grid adjusted dynamically by individual height

--- src/components/image.jsx
+++ src/components/image.jsx
@@ -1,12 +1,29 @@
 import React, { Component } from "react";
 
 class Image extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { height: 0 };
+    this.imageRef = React.createRef(); // creates reference
+  }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener("load", this.adjustHeight);
+  }
+
+  adjustHeight = () => {
+    const height = this.imageRef.current.clientHeight;
+    const adjustedHeight = Math.ceil(height / 10 + 1);
+    this.setState({ height: adjustedHeight })
+  }
+
   render() {
     const { urls, description } = this.props.image;
+    const style = { gridRowEnd: `span ${this.state.height}` };
 
     return (
-      <div className="image">
-        <img src={urls.regular} alt={description} />
+      <div className="image" style={style}>
+        <img ref={this.imageRef} src={urls.regular} alt={description} />
       </div>
     );
   }
